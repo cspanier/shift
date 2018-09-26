40726debeee4439d5619be9168dcaf76fb52bd7d
Christian Spanier <github@boxie.eu>
2018-09-26 22:44:35 +0200

    cmake: rename OSLibs::network to OSLibs::iphlpapi

--- cmake/FindOSLibs.cmake
+++ cmake/FindOSLibs.cmake
@@ -1,22 +1,17 @@
 include(FindPackageHandleStandardArgs)
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-  # add_custom_target(OSLibsNetwork)
-  # add_library(OSLibs::network UNKNOWN IMPORTED)
-  # set_property(TARGET OSLibs::network PROPERTY LINKER_LANGUAGE CXX)
-  # add_dependencies(OSLibs::network OSLibsNetwork)
-
   if(OSLIBS_FOUND)
     set(HAVE_OSLIBS 1)
     set(OSLIBS_INCLUDE_DIRS)
     set(OSLIBS_LIBRARIES)
   endif()
 
-  add_library(OSLibs::network INTERFACE IMPORTED)
-  set_target_properties(OSLibs::network PROPERTIES
+  add_library(OSLibs::iphlpapi INTERFACE IMPORTED)
+  set_target_properties(OSLibs::iphlpapi PROPERTIES
     IMPORTED_LIBNAME "")
 elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
-  find_library(OSLIBS_IPHLPAPI_LIBRARY NAMES IPHLPAPI)
+  find_library(OSLIBS_IPHLPAPI_LIBRARY NAMES iphlpapi.lib)
   mark_as_advanced(OSLIBS_IPHLPAPI_LIBRARY)
 
   FIND_PACKAGE_HANDLE_STANDARD_ARGS(OSLibs REQUIRED_VARS
@@ -29,12 +24,12 @@ elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
   endif()
 
   if(IS_ABSOLUTE "${OSLIBS_IPHLPAPI_LIBRARY}")
-    add_library(OSLibs::network UNKNOWN IMPORTED)
-    set_target_properties(OSLibs::network PROPERTIES
+    add_library(OSLibs::iphlpapi UNKNOWN IMPORTED)
+    set_target_properties(OSLibs::iphlpapi PROPERTIES
       IMPORTED_LOCATION "${OSLIBS_IPHLPAPI_LIBRARY}")
   else()
-    add_library(OSLibs::network INTERFACE IMPORTED)
-    set_target_properties(OSLibs::network PROPERTIES
+    add_library(OSLibs::iphlpapi INTERFACE IMPORTED)
+    set_target_properties(OSLibs::iphlpapi PROPERTIES
       IMPORTED_LIBNAME "${OSLIBS_IPHLPAPI_LIBRARY}")
   endif()
 endif()
