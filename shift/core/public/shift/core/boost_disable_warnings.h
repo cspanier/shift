4fc783350aca80793eec4fcea8137e392921e739
Christian Spanier <github@boxie.eu>
2018-10-08 22:19:23 +0200

    core: ignore more warnings in Boost on MSVC

--- shift/core/public/shift/core/boost_disable_warnings.h
+++ shift/core/public/shift/core/boost_disable_warnings.h
@@ -11,6 +11,9 @@
 #if (BOOST_VERSION <= 106800)
 #pragma warning(push)
 
+// C4005: '<macro-name>': macro redefinition
+#pragma warning(disable : 4005)
+
 // C4245: 'initializing': conversion from '<type>' to '<type>', signed/unsigned
 // mismatch
 #pragma warning(disable : 4245)
@@ -20,6 +23,24 @@
 
 // C4459: declaration of '<var>' hides global declaration.
 #pragma warning(disable : 4459)
+
+// C4996: 'std::allocator<void>': warning STL4009: std::allocator<void> is
+// deprecated in C++17. You can define
+// _SILENCE_CXX17_ALLOCATOR_VOID_DEPRECATION_WARNING or
+// _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS to acknowledge that you have received
+// this warning.
+// C4996: 'std::fpos<_Mbstatet>::seekpos': warning STL4019: The member
+// std::fpos::seekpos() is non-Standard, and is preserved only for compatibility
+// with workarounds for old versions of Visual C++. It will be removed in a
+// future release, and in this release always returns 0. Please use
+// standards-conforming mechanisms to manipulate fpos, such as conversions to
+// and from streamoff, or an integral type, instead. If you are receiving this
+// message while compiling Boost.IOStreams, a fix has been submitted upstream to
+// make Boost use standards-conforming mechanisms, as it does for other
+// compilers. You can define _SILENCE_FPOS_SEEKPOS_DEPRECATION_WARNING to
+// acknowledge that you have received this warning, or define
+// _REMOVE_FPOS_SEEKPOS to remove std::fpos::seekpos entirely.
+#pragma warning(disable : 4996)
 
 // C6001: Using uninitialized memory '<var>'.
 #pragma warning(disable : 6001)
