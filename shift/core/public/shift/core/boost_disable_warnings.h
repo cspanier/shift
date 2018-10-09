c6ec4cd6a557a853927986813127852cf9e3ffda
Christian Spanier <github@boxie.eu>
2018-10-09 17:58:02 +0200

    core: ignore more MSVC warnings in Boost code

--- shift/core/public/shift/core/boost_disable_warnings.h
+++ shift/core/public/shift/core/boost_disable_warnings.h
@@ -24,6 +24,12 @@
 // C4459: declaration of '<var>' hides global declaration.
 #pragma warning(disable : 4459)
 
+// C4706: assignment within conditional expression
+#pragma warning(disable : 4706)
+
+// C4714: function '<symbol>' marked as __forceinline not inlined
+#pragma warning(disable : 4714)
+
 // C4996: 'std::allocator<void>': warning STL4009: std::allocator<void> is
 // deprecated in C++17. You can define
 // _SILENCE_CXX17_ALLOCATOR_VOID_DEPRECATION_WARNING or
@@ -59,15 +65,31 @@
 // C6258: Using TerminateThread does not allow proper thread clean up.
 #pragma warning(disable : 6258)
 
+// C6285: (<non-zero constant> || <non-zero constant>) is always a non-zero
+// constant.  Did you intend to use the bitwise-and operator?
+#pragma warning(disable : 6285)
+
+// C6306: Incorrect call to '<function>': consider using '<function>' which
+// accepts a va_list as an argument.
+#pragma warning(disable : 6306)
+
 // C6323: Use of arithmetic operator on Boolean type(s).
 #pragma warning(disable : 6323)
 
 // C6326: Potential comparison of a constant with another constant.
 #pragma warning(disable : 6326)
 
+// C6385: Reading invalid data from '<var':  the readable size is '<n>' bytes,
+// but '<m>' bytes may be read.
+#pragma warning(disable : 6385)
+
 // C6387: '<var>' could be '<value>':  this does not adhere to the specification
 // for the function '<WinAPI-function>'.
 #pragma warning(disable : 6387)
+
+// C6388: '<var>' might not be '<value>':  this does not adhere to the
+// specification for the function '<function>'.
+#pragma warning(disable : 6388)
 
 // C28251: Inconsistent annotation for '<function>': this instance has no
 // annotations.
