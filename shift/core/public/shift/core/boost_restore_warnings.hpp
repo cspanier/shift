f77113b2f1299666c2010965d7743969c585863e
Christian Spanier <github@boxie.eu>
2019-02-22 19:36:08 +0100

    core: fix wrong preprocessor logic

--- shift/core/public/shift/core/boost_restore_warnings.hpp
+++ shift/core/public/shift/core/boost_restore_warnings.hpp
@@ -1,6 +1,9 @@
 /// @see shift/core/boost_restore_warnings.hpp
+#include <boost/version.hpp>
+#if (BOOST_VERSION <= 106800)
 #if defined(__clang__)
 #pragma clang diagnostic pop
 #elif defined(_MSC_VER)
 #pragma warning(push)
+#endif
 #endif
